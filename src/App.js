fa56f176fad296f8b6c59be12fdcabe1d82a326b
Riley Dallas <rldaggie@gmail.com>
2017-08-25 16:53:02 -0500

    Add logic for sha

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.setSha = this.setSha.bind(this)
+    this.state = {
+      sha: ''
+    }
+  }
+
+  setSha(e) {
+    this.setState({ sha: e.target.value })
+  }
+
+  error() {
+    if (this.state.sha === '') return false;
+  }
+
+  renderSha() {
+    if (this.state.sha === '' || this.error()) return null;
+    return <div>
+      <p>Copy the following code below to paste in your <strong>jupyter_notebook_config.py</strong> file</p>
+      <pre>
+        <code>
+          c = get_config()<br/>
+          c.NotebookApp.certfile = u'/home/ec2-user/ssl/cert.pem'<br/>
+          c.NotebookApp.keyfile = u'/home/ec2-user/ssl/cert.key'<br/>
+          c.IPKernelApp.pylab = 'inline'<br/>
+          c.NotebookApp.ip = '*'<br/>
+          c.NotebookApp.open_browser = False<br/>
+          c.NotebookApp.password = '{this.state.sha}'<br/>
+        </code>
+      </pre>
+    </div>
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div className='container'>
+        <div className="jumbotron">
+          <h1>Jupyter config generator</h1>
+          <p>Paste your <strong>sha:345arga...</strong> code into the text field below.</p>
+          
+          <div className='form-group'>
+            <label>SHA Code</label> 
+            <input 
+              type='text' 
+              className='form-control' 
+              defaultValue={this.state.sha}
+              onChange={this.setSha} />
+          </div>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderSha()}
       </div>
     );
   }
